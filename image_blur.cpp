d3351ba74e0a2c38660762b7e85e805e62b13f2a
Alexander Batashev <alexbatashev@outlook.com>
2020-03-07 14:10:46 +0300

    Less code
    Signed-off-by: Alexander Batashev <alexbatashev@outlook.com>

--- image_blur.cpp
+++ image_blur.cpp
@@ -2,32 +2,7 @@
 #include "stb/stb_image.h"
 
 #include <CL/sycl.hpp>
-
-template <typename T, int KerSize>
-class ImageBlurKernel {
-private:
-  using InpAccT = sycl::accessor<T, 2, sycl::access::mode::read>; 
-  using OutAccT = sycl::accessor<T, 2, sycl::access::mode::discard_write>; 
-  const size_t mImgWidth, mImgHeight;
-  InpAccT mInpImg;
-  OutAccT mOutImg;
-public:
-  ImageBlurKernel(size_t w, size_t h, InpAccT inpAcc, OutAccT outAcc) : 
-      mImgWidth(w), mImgHeight(h), mInpImg(inpAcc), mOutImg(outAcc) {}
-
-  void operator()(sycl::item<2> item) {
-    const auto id = item.get_id();
-    T sum{};
-    for (int x = -KerSize / 2; x < KerSize / 2; x++) {
-      for (int y = -KerSize / 2; y < KerSize / 2; y++) {
-        size_t realX = sycl::clamp(static_cast<int>(id[0]) + x, 0, static_cast<int>(mImgWidth - 1));
-        size_t realY = sycl::clamp(static_cast<int>(id[1]) + y, 0, static_cast<int>(mImgHeight - 1));
-        sum += mInpImg[sycl::id<2>{realX, realY}];
-      }
-    }     
-    mOutImg[id - item.get_offset()] = sum / (KerSize * KerSize);
-  }
-};
+#include <functional>
 
 int main() {
     int w, h, c;
@@ -41,14 +16,18 @@ int main() {
         std::terminate();
     }
 
-    sycl::float3* imageData = new sycl::float3[width*height];
 
-    for (size_t i = 0; i < width*height; i++) {
-      float r, g, b;
-      r = static_cast<float>(image[3*i]) / 255.f;
-      g = static_cast<float>(image[3*i + 1]) / 255.f;
-      b = static_cast<float>(image[3*i + 2]) / 255.f;
-      imageData[i] = sycl::float3{r, g, b};
+    sycl::buffer<sycl::float3, 2> imgBuf{sycl::range<2>{width, height}};
+    sycl::buffer<sycl::float3, 2> outBuf{sycl::range<2>{width, height}};
+    { 
+	    auto imageData = imgBuf.get_access<sycl::access::mode::discard_write>();
+	    for (size_t i = 0; i < width*height; i++) {
+	      float r, g, b;
+	      r = static_cast<float>(image[3*i]) / 255.f;
+	      g = static_cast<float>(image[3*i + 1]) / 255.f;
+	      b = static_cast<float>(image[3*i + 2]) / 255.f;
+	      imageData.get_pointer()[i] = sycl::float3{r, g, b};
+	    }
     }
 
     stbi_image_free(image);
@@ -56,39 +35,39 @@ int main() {
     std::cout << width << std::endl;
     std::cout << height << std::endl;
 
-    sycl::buffer imgBuf{imageData, sycl::range<2>{width, height}};
-    sycl::buffer<sycl::float3, 2> outBuf{sycl::range<2>{width, height}};
-
-    sycl::buffer sub1{outBuf, sycl::id<2>{0, 0}, sycl::range<2>{width/ 2, height}};
-    sycl::buffer sub2{outBuf, sycl::id<2>{width / 2, 0}, sycl::range<2>{width / 2, height}};
+    auto kernelCG = [&](sycl::range<2> range, sycl::id<2> offset, sycl::handler& cgh) {
+        auto inpAcc = imgBuf.get_access<sycl::access::mode::read>(cgh); 
+        auto outAcc = outBuf.get_access<sycl::access::mode::discard_write>(cgh, range, offset);
+	cgh.parallel_for<class ImageBlur>(range, offset, [=](sycl::item<2> item) {
+	    constexpr int KerSize = 3;
+	    const auto id = item.get_id();
+	    sycl::float3 sum{};
+	    for (int x = -KerSize / 2; x < KerSize / 2; x++) {
+	      for (int y = -KerSize / 2; y < KerSize / 2; y++) {
+		size_t realX = sycl::clamp(static_cast<int>(id[0]) + x, 0, static_cast<int>(item.get_range(0) - 1));
+		size_t realY = sycl::clamp(static_cast<int>(id[1]) + y, 0, static_cast<int>(item.get_range(1) - 1));
+		sum += inpAcc[sycl::id<2>{realX, realY}];
+	      }
+	    }     
+	    outAcc[id] = sum / (KerSize * KerSize);
+	});
+    };
 
     try {
       sycl::queue q1{sycl::cpu_selector{}};
       sycl::queue q2{sycl::host_selector{}};
 
-      q1.submit([&](sycl::handler &cgh) {
-        auto inpAcc = imgBuf.get_access<sycl::access::mode::read>(cgh);
-        auto outAcc = sub1.get_access<sycl::access::mode::discard_write>(cgh);
-        ImageBlurKernel<sycl::float3, 2> kernel(width, height, inpAcc, outAcc);
-
-        cgh.parallel_for<decltype(kernel)>(sycl::range<2>{width/2, height}, kernel);
-      });
-
-      q2.submit([&](sycl::handler &cgh) {
-        auto inpAcc = imgBuf.get_access<sycl::access::mode::read>(cgh);
-        auto outAcc = sub2.get_access<sycl::access::mode::discard_write>(cgh);
-        ImageBlurKernel<sycl::float3, 2> kernel(width, height, inpAcc, outAcc);
-
-        cgh.parallel_for<decltype(kernel)>(sycl::range<2>{width/2, height}, sycl::id<2>{width/2, 0}, kernel);
-      });
+      auto cgh1 = std::bind(kernelCG, sycl::range<2>{width/2, height}, sycl::id<2>{0, 0}, std::placeholders::_1);
+      auto cgh2 = std::bind(kernelCG, sycl::range<2>{width/2, height}, sycl::id<2>{width/2, 0}, std::placeholders::_1);
+      q1.submit(cgh1);
+      q1.submit(cgh2);
 
       q1.wait_and_throw();
       q2.wait_and_throw();
     } catch (sycl::exception e) {
+      std::cerr << "Exception caught" << std::endl;
       std::cerr << e.what() << std::endl; 
     }
-
-    delete [] imageData;
 
     return 0;
 }
